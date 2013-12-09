0c819c1fcea0ca54921d50ab70e4e02a6671cf12
Jeff Pritchard <jp@jeffpritchard.com>
2013-12-08 20:26:41 -0500

    add url slug stuff to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,26 @@
 class Photo < ActiveRecord::Base
+  
+  
+  
+  
+  # Our Amazon S3 storage is set up with an equivalent set of both big and small photos to show to the user
+  # The main bucket has two subfolders called big and small
+  # The url_slug we build when we initialize a new "photo" record replaces that "big" or "small" with an @
+  # When we go to get either a big or small version of a photo, we use the class methods below to grab the appropriate real url
+  def self.url_to_url_slug full_url
+    # remove either big or small from the url for generic slug storage
+    url = full_url.gsub("/big", "/@")
+    url = url.gsub("/small", "/@")
+  end
+  
+  def self.url_slug_to_big_url slug_url
+    # replace @ in slug with "big" folder name
+    url = slug_url.gsub("/@", "/big")
+  end
+  
+  def self.url_slug_to_small_url slug_url
+    # replace @ in slug with "small" folder name
+    url = slug_url.gsub("/@", "/small")
+  end
+  
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Photo do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "url_to_url_slug" do
+    
+    it "turns a regular S3 url into a slugged url without the big/small folder" do
+      typical_url = "http://jeffp-images.s3-website-us-east-1.amazonaws.com/big/foo.jpg"    
+      slug = Photo::url_to_url_slug(typical_url)    
+      # pp slug    
+      expect(slug).not_to match("big")
+      expect(slug).not_to match("small")
+      expect(slug).to match("@")
+    end
+
+  end
+
+  describe "url_slug_to_big_url" do
+    
+    it "turns a url slug into a real and usable url" do
+      typical_slug_url = "http://jeffp-images.s3-website-us-east-1.amazonaws.com/@/foo.jpg"    
+      url = Photo::url_slug_to_big_url(typical_slug_url)    
+      # pp url    
+      expect(url).not_to match("@")
+      expect(url).to match("big")
+    end
+
+  end
+
+  describe "url_slug_to_small_url" do
+    
+    it "turns a url slug into a real and usable url" do
+      typical_slug_url = "http://jeffp-images.s3-website-us-east-1.amazonaws.com/@/foo.jpg"    
+      url = Photo::url_slug_to_small_url(typical_slug_url)    
+      # pp url    
+      expect(url).not_to match("@")
+      expect(url).to match("small")
+    end
+
+  end
+
+
+
 end
