e14ce3e643db9336e2a91cbf48fb49e50affe377
Jeff Pritchard <jp@jeffpritchard.com>
2013-12-09 17:56:00 -0500

    after adding photo factory

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,32 @@
+require_relative "../../lib/amazon/bucket.rb"
+
+
 class Photo < ActiveRecord::Base
   
   
-  
+  # photo_factory is a class method that looks at our Amazon S3 storage and builds a photo record for each file there
+  # any files that are already represented by a photo record are of course skipped
+  # it makes the brash assumption that if the number of files on S3 is equal to number of records there is nothing to do
+  #(that assumption could break with careless deletion and addition of equal number of files between calls -- unlikely)
+  def self.photo_factory
+    image_bucket = Amazon::Bucket.new('jeffp-images')
+    unless image_bucket.nil?
+      files = image_bucket.get_files_in_folder("big")
+            
+      if files.size > Photo.all.count
+        files.each do |one_file|
+          located_photo = Photo.where(:file_name => one_file).first
+          unless located_photo
+            # pp "making a photo for file #{one_file}"
+            temp = Photo.create(:file_name => one_file)
+          end
+        end
+      end
+      
+    else
+      pp "Something very bad happened in the photo factory...couldn't find our image bucket"
+    end
+  end
   
   # Our Amazon S3 storage is set up with an equivalent set of both big and small photos to show to the user
   # The main bucket has two subfolders called big and small
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require_relative "../../lib/amazon/bucket.rb"
 
 describe Photo do
 
@@ -39,6 +40,30 @@ describe Photo do
 
   end
 
+
+  describe "photo_factory" do
+    let(:bucket1) {Amazon::Bucket.new('jeffp-images')}
+    
+    it "creates photo records for any orphaned images on Amazon S3 storage" do
+      # first verify that we have some images and no photo records
+      files = bucket1.get_files_in_folder("big")
+      expect(files).not_to be_empty
+      photo_count = Photo.all.count
+      expect(photo_count).to eq(0)
+      
+      # now call the factory and verify that we have a photo record for each file
+      Photo::photo_factory
+      photo_count = Photo.all.count
+      expect(photo_count).to eq(files.size)
+      
+      # now verify that it doesn't re-create it again if we call factory again
+      Photo::photo_factory
+      photo_count = Photo.all.count
+      expect(photo_count).to eq(files.size)
+      
+    end
+    
+  end
 
 
 end
