4d9c256b6f8acd324deeafcbaf3bed00879e3852
Jeff Pritchard <jp@jeffpritchard.com>
2013-12-16 15:20:57 -0500

    show file name in large view

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -14,6 +14,16 @@ module PhotosHelper
     uri = "/photos?active_tab=large&page_large=#{index_in_all_array.to_s}"
   end
   
+  def get_x_of_y(photo, all_photos)
+    # first we need to figure out where this image lives in the list of pictures selected by the filters
+    index_in_all_array = all_photos.to_a.index(photo)
+        
+    # array index is zero based but our pages are one based
+    index_in_all_array += 1
+  
+    # build the x of y string
+    output = "<b>&nbsp;&nbsp;&nbsp;#{index_in_all_array.to_s} of #{all_photos.count.to_s} &nbsp;&nbsp;&nbsp;</b>"
+  end
   
   
 end
